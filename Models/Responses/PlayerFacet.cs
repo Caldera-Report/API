254d9025d4682ab94a85f5a819257b64d12ebcd5
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-09-29 20:14:26 -0400

    #21 Fixing issue where player display name would be 1 digit short of the full code

--- Models/Responses/PlayerFacet.cs
+++ Models/Responses/PlayerFacet.cs
@@ -8,7 +8,7 @@ namespace API.Models.Responses
     {
         public static void Map(Player source, PlayerDto target)
         {
-            target.FullDisplayName = $"{source.DisplayName}#{source.DisplayNameCode}";
+            target.FullDisplayName = source.DisplayNameCode > 1000 ? $"{source.DisplayName}#{source.DisplayNameCode}" : $"{source.DisplayName}#0{source.DisplayNameCode}";
         }
     }
 
