a7941fd25d23cc55fc3286da8d19a2938ec18312
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-10-30 22:30:21 -0400

    Dropping unused Id column from activityreportplayers

---
+++ Domain/Migrations/20251031022944_RemoveIdFromActivityReportPlayer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIdFromActivityReportPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "ActivityReportPlayers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "ActivityReportPlayers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
