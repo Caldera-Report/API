4e21f3f7dfc190a549faeb86b99cac19d4401ffd
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-11-02 16:22:41 -0500

    Fixing unit test failures

--- API/Functions/PlayerFunctions.cs
+++ API/Functions/PlayerFunctions.cs
@@ -88,7 +88,7 @@ public class PlayerFunctions
 
         try
         {
-            var players = await _queryService.GetAllPlayersFromDb();
+            var players = await _queryService.GetAllPlayersAsync();
             return ResponseHelpers.CachedJson(req, players, _jsonOptions, 0);
         }
         catch (Exception ex)
